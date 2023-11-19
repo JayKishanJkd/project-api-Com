f8e9b359b283eaa72590d0e30df939209cb9f31a
JayKishanJkd <jaykishandaiya438@gmail.com>
2023-11-19 13:09:44 +0530

    Initial commit

--- index.js
+++ index.js
@@ -12,7 +12,35 @@ app.use(bodyParser.json());
 
 // Replace these connection details with your PostgreSQL configuration
 const pool = new Pool({
-    connectionString: 'postgresql://ultv6k63uq36vroaucej:RtVoGYJLt04avjXaTrcp7v5xDW04l4@b17shpbqy6dfakrwipw9-postgresql.services.clever-cloud.com:50013/b17shpbqy6dfakrwipw9',
+  connectionString: 'postgresql://ultv6k63uq36vroaucej:RtVoGYJLt04avjXaTrcp7v5xDW04l4@b17shpbqy6dfakrwipw9-postgresql.services.clever-cloud.com:50013/b17shpbqy6dfakrwipw9',
+});
+
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const client = await pool.connect();
+
+    // Check if the user with the provided email already exists
+    const result = await client.query('SELECT * FROM jkauth.users WHERE email = $1', [email]);
+
+    if (result.rows.length > 0) {
+      client.release();
+      res.status(400).json({ error: 'User with this email already exists' });
+      return;
+    }
+
+    // If the user doesn't exist, create a new user
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await client.query('INSERT INTO jkauth.users (email, password) VALUES ($1, $2) RETURNING *', [email, hashedPassword]);
+
+    client.release();
+    
+    res.json({ message: 'Registration successful' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 app.post('/login', async (req, res) => {
@@ -25,27 +53,25 @@ app.post('/login', async (req, res) => {
     const result = await client.query('SELECT * FROM jkauth.users WHERE email = $1', [email]);
 
     if (result.rows.length === 0) {
-      // If the user doesn't exist, create a new user
-      const hashedPassword = await bcrypt.hash(password, 10);
-      await client.query('INSERT INTO jkauth.users (email, password) VALUES ($1, $2)', [email, hashedPassword]);
-
+      // If the user doesn't exist, return an error
       client.release();
-      
-      res.json({ message: 'Registration successful' });
+      res.status(401).json({ error: 'User not found' });
+      return;
+    }
+
+    // If the user exists, check the password for login
+    const storedPasswordHash = result.rows[0].password;
+    const passwordMatch = await bcrypt.compare(password, storedPasswordHash);
+
+    if (!passwordMatch) {
+      // If the password doesn't match, return an error
+      res.status(401).json({ error: 'Invalid credentials' });
     } else {
-      // If the user exists, check the password for login
-      const storedPasswordHash = result.rows[0].password;
-      const passwordMatch = await bcrypt.compare(password, storedPasswordHash);
-
-      if (!passwordMatch) {
-        res.status(401).json({ error: 'Invalid credentials' });
-        return;
-      }
-
-      client.release();
-
+      // If login is successful, return a success message
       res.json({ message: 'Login successful' });
     }
+
+    client.release();
   } catch (err) {
     console.error(err);
     res.status(500).send('Internal Server Error');
